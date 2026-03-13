8bc8efbcbe9ce088f9511d0f4c4823fe03fe655f
Chuck <cg9473150@gmail.com>
2026-03-13 16:51:22 +0100

    added intro and first lecture.

---
+++ 0_Introduction/KI.mm
@@ -0,0 +1,114 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="KI" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1773393288165"><hook NAME="MapStyle" background="#2e3440ff" zoom="1.1">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" auto_compact_layout="true" show_tags="UNDER_NODES" show_note_icons="true" associatedTemplateLocation="template:/dark_nord_template.mm" fit_to_viewport="false" show_icons="BESIDE_NODES" showTagCategories="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_671184412" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#eceff4" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#88c0d0" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_671184412" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" BORDER_WIDTH="1.9 px">
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ebcb8b">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#484747">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#5e81ac"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_779275544" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_779275544"/>
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#eceff4">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font SIZE="20" BOLD="true"/>
+<node TEXT="1. Logik- und regelbasierte KI" POSITION="bottom_or_right" ID="ID_1355017035" CREATED="1773334890414" MODIFIED="1773335039645" HGAP_QUANTITY="98.75 pt" VSHIFT_QUANTITY="-16.5 pt">
+<node TEXT="Aussagenlogik" ID="ID_1503694867" CREATED="1773334915778" MODIFIED="1773334937154"/>
+<node TEXT="Prädikatenlogik erster Stufe" ID="ID_1944183224" CREATED="1773334941194" MODIFIED="1773334956679"/>
+</node>
+<node TEXT="2. Wissensrepräsentation" POSITION="bottom_or_right" ID="ID_360423600" CREATED="1773334962735" MODIFIED="1773343360784" HGAP_QUANTITY="95.75 pt" VSHIFT_QUANTITY="-18.75 pt">
+<node TEXT="Ontologien" ID="ID_1984085168" CREATED="1773334996646" MODIFIED="1773335002383"/>
+<node TEXT="Knowledge graphs" ID="ID_1391254640" CREATED="1773335009124" MODIFIED="1773335016914"/>
+<node TEXT="Symbolische KI" ID="ID_622395994" CREATED="1773335018675" MODIFIED="1773335025711"/>
+</node>
+<node TEXT="Maschinelles Lernen (ML)" POSITION="bottom_or_right" ID="ID_1331629695" CREATED="1773335057765" MODIFIED="1773344658705" HGAP_QUANTITY="113 pt" VSHIFT_QUANTITY="21 pt">
+<node TEXT="3. Überwachtes ML" ID="ID_136825166" CREATED="1773335104485" MODIFIED="1773335125223" HGAP_QUANTITY="34.25 pt" VSHIFT_QUANTITY="12 pt">
+<node TEXT="Lineare Regression *" ID="ID_644769865" CREATED="1773335146008" MODIFIED="1773335277057"/>
+<node TEXT="Artificial Neural Networks" ID="ID_1105359856" CREATED="1773335160112" MODIFIED="1773335174310"/>
+<node TEXT="Extreme Learning Machines (ELMs)" ID="ID_1166686133" CREATED="1773335179122" MODIFIED="1773335203260"/>
+</node>
+<node TEXT="4. Unüberwachtes ML" ID="ID_663764597" CREATED="1773335213523" MODIFIED="1773343411993" HGAP_QUANTITY="35 pt" VSHIFT_QUANTITY="25.5 pt">
+<node TEXT="Clustering (k-means) *" ID="ID_552312668" CREATED="1773335284660" MODIFIED="1773335303868"/>
+<node TEXT="Graph Neural Networks (GNNs)" ID="ID_997882928" CREATED="1773343382229" MODIFIED="1773343397889"/>
+</node>
+<node TEXT="5. Verstärkungslernen (RL)" ID="ID_1162299590" CREATED="1773343414717" MODIFIED="1773343451620" HGAP_QUANTITY="31.25 pt" VSHIFT_QUANTITY="27 pt">
+<node TEXT="Markov-Entscheidungsprozess*" ID="ID_1001301309" CREATED="1773343463556" MODIFIED="1773343485898"/>
+<node TEXT="Policy-based learning" ID="ID_18410152" CREATED="1773343487574" MODIFIED="1773343499575"/>
+</node>
+<node TEXT="6. Neurosymbolische KI" ID="ID_1257988240" CREATED="1773343515356" MODIFIED="1773343530493" HGAP_QUANTITY="34.25 pt" VSHIFT_QUANTITY="48.75 pt">
+<node TEXT="Erklärbare KI (XAI)" ID="ID_1639721200" CREATED="1773343538084" MODIFIED="1773343566329"/>
+<node TEXT="Vector Symbolic Architectures (VSAs)" ID="ID_707165185" CREATED="1773344114370" MODIFIED="1773344144961"/>
+</node>
+</node>
+</node>
+</map>
